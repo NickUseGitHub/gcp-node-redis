456a87e49d2b127f7a689391c3d88f7fcc3dd680
Chaiwtun <chaiwtun@scg.com>
2020-02-16 21:58:31 +0700

    [init-terraform] remove unused export

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Response, Request } from "express";
+import express from "express";
 import bodyParser from "body-parser";
 import { ApiInterface } from "./controllers/apiTypes";
 
